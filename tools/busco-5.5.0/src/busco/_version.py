3f08b2200028cd8cf39edffb079d17bafa08ccee
Andrew Boylan <andreweboylan@gmail.com>
2023-12-28 17:38:59 -0500

    Remove tools folder from repository

--- tools/busco-5.5.0/src/busco/_version.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# coding: utf-8
-"""
-
-Copyright (c) 2016-2023, Evgeny Zdobnov (ez@ezlab.org)
-Licensed under the MIT license. See LICENSE.md file.
-
-"""
-__version__ = "5.5.0"
